43c3edaacef7059482b6891fa790741c837d5025
emanuel <emanuelbalcazar13@gmail.com>
2018-03-23 17:09:39 -0300

    Correccion de formato

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { App } from './app/app';
 
 // start http server.
 App.listen(App.get("port"), () => {
-	console.log("[*] - App is running at http://%s:%s", App.get("host"), App.get("port"));
+    console.log("[*] - App is running at http://%s:%s", App.get("host"), App.get("port"));
 });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,7 +4,7 @@ import { Router, Request, Response } from 'express';
 const router: Router = Router();
 
 router.get('/info', (req: Request, res: Response) => {
-	res.send({ name: "vuejs express typescript", version: "1.0", date: "March 2018" });
+    res.send({ name: "vuejs express typescript", version: "1.0", date: "March 2018" });
 });
 
 export const Routes = router;
